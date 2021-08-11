161a1c933e3792a5098a9573918a322c3f4c9ff9
FARDIN98 <fardinahmed66@gmail.com>
2021-08-11 22:18:37 +0600

    C++ codes

---
+++ 1st_C_Programme/1st_code.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "hello world!";
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<conio.h>
+// using namespace std;
+
+// int main(){
+//     cout << "Hello World";
+//     getch();
+// }
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    cout << "Fardin" << " Ehsan" << " Ahmed";
+    getch();
+}
+
+// Output: Fardin Ehsan Ahmed
---
+++ Escape_Sequence/Escape_Sequence.cpp
@@ -0,0 +1,52 @@
+// #include<iostream>
+// #include<conio.h>
+// using namespace std;
+
+// int main(){
+//     cout << " Fardin\n Ehsan\n Ahmed";
+//     getch();
+// }
+// Output: Fardin
+        // Ehsan
+        // Ahmed
+
+
+
+// #include<iostream>
+// #include<conio.h>
+// using namespace std;
+
+// int main(){
+//     cout << "Fardin Ehsan Ahmed\n";
+//     cout << "Bashabo\n";
+//     cout << "01533989026";
+//     getch();
+// }
+
+// Output: Fardin Ehsan Ahmed
+//         Bashabo
+//         01533989026
+
+// #include<iostream>
+// #include<conio.h>
+// using namespace std;
+
+// int main(){
+//     cout << "Fardin Ehsan Ahmed" << endl;
+//     cout << "Bashabo" << endl;
+//     cout << "01533989026";
+//     getch();
+// }
+
+// Output: Fardin Ehsan Ahmed
+//         Bashabo
+//         01533989026
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    cout << "Fardin" <<endl << "Bashabo" <<endl << "01533989026";
+    getch();
+}
